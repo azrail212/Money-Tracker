689469134983eef92f1cb0e8c3f5bc77b66c8892
azrail212 <becirovicazra00@gmail.com>
2021-06-06 16:13:05 +0200

    Added user entity & userdao

---
+++ app/src/main/java/com/example/moneytracker/Entities/User.java
@@ -0,0 +1,53 @@
+package com.example.moneytracker.Entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "users")
+public class User {
+
+    @PrimaryKey (autoGenerate = true)
+    private long id;
+
+    private String name;
+    private String username;
+    private String password;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
