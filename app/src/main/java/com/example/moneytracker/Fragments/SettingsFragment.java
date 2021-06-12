f50285da3aa661ff442c22ac397d82fbe7b3ca08
azrail212 <becirovicazra00@gmail.com>
2021-06-12 14:45:59 +0200

    settings layout completed

--- app/src/main/java/com/example/moneytracker/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/moneytracker/Fragments/SettingsFragment.java
@@ -1,9 +1,13 @@
 package com.example.moneytracker.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +16,22 @@ import androidx.fragment.app.Fragment;
 import com.example.moneytracker.R;
 
 public class SettingsFragment extends Fragment {
+    private ImageView profilePicture;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings,container,false);
-
-
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        profilePicture = view.findViewById(R.id.profile_picture);
+        return view;
     }
+
+    public void onChangeProfilePictureButtonClick(View view) {
+        Intent intent = new Intent();
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setType("image/*");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
